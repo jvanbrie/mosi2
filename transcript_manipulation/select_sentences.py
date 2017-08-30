25c270cb68a7cd7db024701b696ed4e9f90aaa04
jvanbrie <jvanbrie@andrew.cmu.edu>
2017-08-30 16:19:55 -0400

    adding sentiment output file and updated script to select sentences

--- transcript_manipulation/select_sentences.py
+++ transcript_manipulation/select_sentences.py
@@ -24,49 +24,53 @@ def main(args):
 		cid = line_list[0]
 		vid = line_list[1]
 		try:
-			channels[cid] += [one_line]
+			channels[cid] += [line_list]
 		except:
-			channels[cid] = one_line
+			channels[cid] = [line_list]
 		one_line = input_file.readline()
 		line_list = one_line.split(',')
 
 	print(len(channels))
 
 	list_sent_dicts = [None] * 5
+	list_selected_sents = [None] * 5
+	chans_to_delete = set([])
 
 	num_chans = len(channels)
 	for i in range(0,5):
 		list_sent_dicts[i] = copy.deepcopy(channels)
-		sent_level = i+1
+		sent_level = i
+		chans_to_delete = set([])
 
-		avg_sentences_needed = int((float(target) / float(5)) / float(num_chans))
-		sentences_of_one_sent
-		for chan in list_sent_dicts[i]:
-			sentences_taken = []
-			for sent in list_sent_dicts[i][chan]:
-				if(len(sentences_taken) >= avg_sentences_needed):
-					break
-				#checks if the given sentence has the sentiment that we're currently looking at
-				if(sent[3] == sent_level):
-					sentences_taken += [sent]
-			if(len(sentences_taken) < avg_sentences_needed):
-				del list_sent_dicts[i][chan]
+		avg_sentences_needed = int((float(args.target) / float(5)) / float(num_chans))
+		print(avg_sentences_needed)
+		sentences_taken_sent = []
+		while(len(sentences_taken_sent) < int((float(args.target) / float(5)))):
+			if(len(list_sent_dicts[i]) == 0):
+				print("wtf")
+				break
+			avg_sentences_needed = max(int((float((int(args.target) - len(sentences_taken_sent))) / float(5)) / float(len(list_sent_dicts[i]))),1)
+			for chan in list_sent_dicts[i]:
+				sentences_taken_chan = 0
+				for sent in list_sent_dicts[i][chan]:
+					if(sentences_taken_chan >= avg_sentences_needed):
+						break
+					#checks if the given sentence has the sentiment that we're currently looking at
+					if(sent[2] == str(sent_level)):
+						sentences_taken_sent.insert(0,sent)
+						sentences_taken_chan += 1
+				if(sentences_taken_chan < avg_sentences_needed):
+					chans_to_delete.add(chan)
+				# if(sentences_taken_sent >= avg_sentences_needed):
+				# 	break
+			print(len(chans_to_delete))
+			for chan in chans_to_delete:
+				(list_sent_dicts[i]).pop(chan,None)
+		list_selected_sents[i] = sentences_taken_sent
+		print("number of sents found: %d" % len(list_selected_sents[i]))
 
 
 		#remove when one iteration works as expected
-		break
-
-
-
-
-
-
-
-
-
-
-
-
 
 def parse_args():
 	parser = argparse.ArgumentParser(description="""Take in files of transcript
